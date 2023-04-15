856dc690c5ba1137e3f764d4309576370f3a48a3
Kasoulides <ckasou01@ucy.ac.cy>
2023-04-15 19:54:05 +0300

    Illumination Optimization problem comments added

--- Round_A/Illumination Optimization.py
+++ Round_A/Illumination Optimization.py
@@ -1,39 +1,53 @@
-def solve_case(m, r, x):
-    n = len(x)
-    i, j = 0, 0
-    count = 0
-
-    while i < n:
-        j = i
-        while j < n and x[j] - x[i] <= r:
-            j += 1
-
-        if j == i:
-            return "IMPOSSIBLE"
-
-        k = j - 1
-        count += 1
-
-        if k == n - 1:
-            return count
-
-        while k < n - 1 and x[k + 1] - x[j - 1] <= r:
-            k += 1
-
-        if k == n - 1 and x[k] + r >= m:
-            return count
-
-        if j == k + 1 and x[i] - r <= 0 and (k == n - 1 or x[k + 1] - x[k] > r) or x[k] + r >= m:
-            return count
-
-        i = k + 1
-
-    return count
-
-
+# read number of test cases
 t = int(input())
-for case in range(1, t + 1):
+
+# iterate over test cases
+for case in range(1, t+1):
+    # read input values for this test case
     m, r, n = map(int, input().split())
     x = list(map(int, input().split()))
-    result = solve_case(m, r, x)
-    print("Case #{}: {}".format(case, result))
+
+    # initialize variables
+    i = 0  # leftmost streetlight without bulb
+    count = 0  # number of bulbs needed
+    j = -1  # rightmost bulb installed
+
+    # iterate over streetlights
+    while i < n:
+        # find rightmost streetlight that can be illuminated by a new bulb
+        while j + 1 < n and x[j+1] - x[i] <= r:
+            j += 1
+
+        # if no such streetlight exists, it is impossible to illuminate the entire freeway
+        if j == i - 1:
+            print("Case #{}: IMPOSSIBLE".format(case))
+            break
+
+        # install bulb at the rightmost streetlight found
+        count += 1
+
+        # find rightmost streetlight that can be illuminated by the new bulb
+        k = j
+        while k + 1 < n and x[k+1] - x[j] <= r:
+            k += 1
+
+        # check if we have covered the entire freeway
+        if j == k and (x[i] - r <= 0 or x[j] + r >= m):
+            print("Case #{}: {}".format(case, count))
+            break
+
+        # move to the next streetlight to be covered
+        i = k + 1
+
+        # check if we need to install another bulb to cover this next streetlight
+        if i < n and x[i] - x[j] > r:
+            count += 1
+            j = k
+
+        # if we don't need to install another bulb, we move the rightmost bulb to the end of the current range
+        else:
+            j = k
+
+    # if we didn't break from the loop, it means we covered all streetlights and the entire freeway
+    else:
+        print("Case #{}: {}".format(case, count))
